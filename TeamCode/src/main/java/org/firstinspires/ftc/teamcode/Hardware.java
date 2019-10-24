cd34cfac3949569cd18b3f9c794792ed63953b61
Csun01 <42942891+Csun01@users.noreply.github.com>
2019-10-23 19:56:13 -0500

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,289 +1,150 @@
 package org.firstinspires.ftc.teamcode;
 
-
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.CRServo;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 
 
-
-
-/**
-
- * Created by Lance He 9/16/2017.
-
- */
-
-
-
-public class Hardware {
-
-
-
-    // Motor variable names
-
+public class Hardware extends OpMode {
     public DcMotor frontLeftMotor = null;
-
     public DcMotor frontRightMotor = null;
-
     public DcMotor backLeftMotor = null;
-
     public DcMotor backRightMotor = null;
-    public CRServo clawServo = null;
-
-
-
-
-
-    // Other variable names
+    
+    //Vuforia Key Fragment
+    private static final String VUFORIA_KEY = "AY7Mgrn/////AAABmfI+bBY2KUjrj+QipjjQqMZkBy9G2EFK3wmdSctymzd4yHsn91iwwOFsV+MCDHbeaRmEFzBv3MqjDt3prE4YDrozJIUKha18nK9zZVHLMJvE7EE2nyQ2W/DTw045lQDRjRuNjuALhnd9RePTFlNJar/yqkADNUAPR+WNlT+Yb2R29d87B8Q352a7kMNXzglLi+yHtBg1fJwygyuxp8vhlgMd5OFRyBEn3VAtqFxpEVJz5AfdQ6wgKIMwJhSONcHuNmEO6Rvp+QDzKDGtdQ69o2WpRHb9auU/mwTFCjY18ZowZoTfihgoZOHL/g4a4uFOiHqbEQNUNTeulQmVRUuh8iWy2OBwf/Ec/uCEM+RiKzxz";
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Stone";
+    private static final String LABEL_SECOND_ELEMENT = "Skystone";
+    // Initialzing both vuforia and tfod
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
 
     HardwareMap hwMap;
-
     private ElapsedTime period = new ElapsedTime();
 
-
-
-    public Hardware()
-
-    {
-
+    public Hardware(){
         hwMap = null;
-
     }
+    @Override
+    public void init() { }
 
+    @Override
+    public void loop() { }
 
-
-    public void init(HardwareMap hwMap)
-
-    {
-
-
-
-        // Save reference to Hardware map
-
+    public void init(HardwareMap hwMap, Telemetry telemetry) {
         this.hwMap = hwMap;
 
-        period.reset();
-
-
-
-        // Define Motors
-
         frontLeftMotor = hwMap.dcMotor.get("left_front");
-
         frontRightMotor = hwMap.dcMotor.get("right_front");
-
         backLeftMotor = hwMap.dcMotor.get("left_back");
-
         backRightMotor = hwMap.dcMotor.get("right_back");
 
-
-        //frontLeftMotor = hwMap.dcMotor.get("front_left");
-
-        //frontRightMotor = hwMap.dcMotor.get("front_right");
-
-        //backLeftMotor = hwMap.dcMotor.get("back_left");
-
-        //backRightMotor = hwMap.dcMotor.get("back_right");
-
-
-        // Initialize Motors
-
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-
-        // ******MAY CHANGE *******  Fix Forward/Reverse under testing
-
-        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
-
-
-
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         frontLeftMotor.setPower(0);
-
         frontRightMotor.setPower(0);
-
         backLeftMotor.setPower(0);
-
         backRightMotor.setPower(0);
-
-        clawServo = hwMap.crservo.get("intake_servo");
-
-
-
-    }
-
-
-
-    public void initTeleOpDelayIMU(HardwareMap hwMap){
-
-        // Save reference to Hardware map
-
-        this.hwMap = hwMap;
-
-        period.reset();
-
-
-        // Define Motors
-
-        frontLeftMotor = hwMap.dcMotor.get("left_front");
-
-        frontRightMotor = hwMap.dcMotor.get("right_front");
-
-        backLeftMotor = hwMap.dcMotor.get("left_back");
-
-        backRightMotor = hwMap.dcMotor.get("right_back");
-
-
-
-        // Initialize Motors
-
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-
-
-
-        // ******MAY CHANGE *******  Fix Forward/Reverse under testing
-
-        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
-
-
-
-        frontLeftMotor.setPower(0);
-
-        frontRightMotor.setPower(0);
-
-        backLeftMotor.setPower(0);
-
-        backRightMotor.setPower(0);
-
-
-
-        // May use RUN_USING_ENCODERS if encoders are installed
 
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
         frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+        public void init_auto(HardwareMap hwMap, Telemetry telemetry){
+        init(hwMap, telemetry);
+            frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+            frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+            backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            backRightMotor.setDirection(DcMotor.Direction.FORWARD);
 
 
+            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+            parameters.vuforiaLicenseKey = VUFORIA_KEY;
+            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+
+            vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+            if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+                initTfod();
+            } else {
+                telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+            }
+
+
+            int tfodMonitorViewId = hwMap.appContext.getResources().getIdentifier(
+                    "tfodMonitorViewId", "id", hwMap.appContext.getPackageName());
+            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+            tfodParameters.minimumConfidence = 0.8;
+            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, this.vuforia);
+            tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
 
     }
 
+    public void initVuforia() {
 
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
 
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
-    public void initTeleOpNOIMU(HardwareMap hwMap){
-
-        // Save reference to Hardware map
-
-        this.hwMap = hwMap;
-
-        period.reset();
-
-
-
-        // Define Motors
-
-        frontLeftMotor = hwMap.dcMotor.get("left_front");
-
-        frontRightMotor = hwMap.dcMotor.get("right_front");
-
-        backLeftMotor = hwMap.dcMotor.get("left_back");
-
-        backRightMotor = hwMap.dcMotor.get("right_back");
-
-
-
-
-        // Initialize Motors
-
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-
-
-
-        // ******MAY CHANGE *******  Fix Forward/Reverse under testing
-
-        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-
-        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
-
-
-
-        frontLeftMotor.setPower(0);
-
-        frontRightMotor.setPower(0);
-
-        backLeftMotor.setPower(0);
-
-        backRightMotor.setPower(0);
-
-
-        // May use RUN_USING_ENCODERS if encoders are installed
-
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
     }
 
-
-
+    // Loading Models ....
+    public void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
     public double getTime(){
-
         return period.time();
-
     }
+
+    public void resetTime(){
+        period.reset();
+    }
+    public void startTime(){
+        period.startTime();
+    }
+
 
 
 }
