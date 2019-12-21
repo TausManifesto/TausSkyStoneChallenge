34684ba9375635eac91ddd6979002452bc5dbee0
Csun01 <42942891+Csun01@users.noreply.github.com>
2019-12-20 19:26:45 -0600

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Mechanum_Foundation_Blue_Wall.java
@@ -0,0 +1,42 @@
+//code for the autonomous portion of the Skystone challenge
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+/**
+ * created by ashwin jandhyala
+ * 10/6/2019
+ */
+//@Disabled
+@Autonomous(name = "Foundation_Blue_Wall", group = "Tau")
+
+public class Auto_Mechanum_Foundation_Blue_Wall extends Auto_Methods {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //initializing robot
+        initRobot();
+
+        //after start is pressed
+
+        //red -- foundation for adjacent to wall
+        liftUp(.5, 1000);
+        strafeRight(.5,8);
+        backward(.5, 35);
+        liftDrop();
+        forward(.5, 27);
+        left(90);
+        liftUp(.5,1000);
+        strafeRight(.5,10);
+        forward(.5,15);
+        liftDrop();
+        forward(.5,25);
+
+
+
+
+    }
+}
